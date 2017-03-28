97f2d56d4ec9c5c7f89f5f8e714cbdc7938e3180
Dylan McDonald <dmcdonald21@cnm.edu>
2017-03-27 21:43:26 -0600

    added app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: 'dylanandtonyfiesta-app',
+	templateUrl: './templates/dylanandtonyfiesta-app.php'
+})
+
+export class AppComponent {}
