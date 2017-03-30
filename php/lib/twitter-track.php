097e1c101b763bd615103ea857f76f9a38a8b44f
Dylan McDonald <dmcdonald21@cnm.edu>
2017-03-29 22:44:33 -0600

    added starter tracker

---
+++ php/lib/twitter-track.php
@@ -0,0 +1,24 @@
+<?php
+require_once("/etc/apache2/encrypted-config/encrypted-config.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use OwlyCode\StreamingBird\StreamReader;
+use OwlyCode\StreamingBird\StreamingBird;
+
+try {
+	$config = readConfig("/etc/apache2/encrypted-config/dylanandtonyfiesta.ini");
+	$twitter = json_decode($config["twitter"]);
+	var_dump($twitter);
+
+	$bird = new StreamingBird($twitter->consumerKey, $twitter->consumerSecret, $twitter->accessToken, $twitter->accessTokenSecret);
+
+	$bird
+		->createStreamReader(StreamReader::METHOD_FILTER)
+		->setTrack(["#luzverde"])
+		->consume(function ($tweet) {
+			echo "------------------------" . PHP_EOL;
+			echo json_encode($tweet) . PHP_EOL;
+		});
+} catch(Exception $exception) {
+	echo "Exception: " . $exception->getMessage() . PHP_EOL;
+}
