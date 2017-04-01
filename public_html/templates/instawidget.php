a504981ea5fa72bf472fc905fe2ed30d6b437a57
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-01 14:24:32 -0600

    added InstaWidget template

---
+++ public_html/templates/instawidget.php
@@ -0,0 +1,2 @@
+<a href="https://instawidget.net/v/tag/dylanandtonyfiesta" id="link-ca773e4c4c96e5924dcd692b2df0d5c5fb3f5677101a8ecac0bb6c5e3d90207a">#dylanandtonyfiesta</a>
+<script src="https://instawidget.net/js/instawidget.js?u=ca773e4c4c96e5924dcd692b2df0d5c5fb3f5677101a8ecac0bb6c5e3d90207a&width=800px"></script>
