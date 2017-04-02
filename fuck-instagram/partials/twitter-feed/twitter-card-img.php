c4204c108b2166efba3d298fefe83ff6bec9f353
Skyler Rexroad <skyrex1095@gmail.com>
2017-04-02 14:29:04 -0600

    Restrucured card img

--- fuck-instagram/partials/twitter-feed/twitter-card-img.php
+++ fuck-instagram/partials/twitter-feed/twitter-card-img.php
@@ -2,9 +2,8 @@
 	<h2 class="card-title"><a href="https://twitter.com/Skylarity">@Skylarity</a></h2>
 	<div class="card-body">
 		<div class="card-image">
-			<img class="img-responsive" src="http://placehold.it/500x500" alt="">
+			<img class="img-responsive center-block" src="http://placehold.it/500x500" alt="">
 		</div>
-		<hr>
 		<div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="https://twitter.com/search?f=tweets&q=%23DylanAndTonyFiesta&src=typd">#DylanAndTonyFiesta</a></div>
 	</div>
 	<div class="card-footer">
