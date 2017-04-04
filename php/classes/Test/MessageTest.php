b5e42aa961a3c2b6d2f13adab57b113be28cc303
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-04 12:51:32 -0600

    added test method to test transporters

--- php/classes/Test/MessageTest.php
+++ php/classes/Test/MessageTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Deepdivedylan\DylanAndTonyFiesta\Test;
 
-use Deepdivedylan\DylanAndTonyFiesta\{Message, Profile};
+use Deepdivedylan\DylanAndTonyFiesta\{Message, Media, Profile};
 
 require_once(dirname(__DIR__) . "/autoload.php");
 
@@ -76,6 +76,35 @@ class MessageTest extends DylanAndTonyFiestaTest {
 		$this->assertEquals($pdoMessage->getMessageProfileId(), $message->getMessageProfileId());
 		$this->assertEquals($pdoMessage->getMessageContent(), $message->getMessageContent());
 		$this->assertEquals($pdoMessage->getMessageDateTime(), $message->getMessageDateTime());
+	}
+
+	/**
+	 * tests beaming out the away team
+	 **/
+	public function testBeamOutMessages() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("message");
+
+		$message = new Message($this->VALID_MESSAGEID, $this->profile->getProfileId(), $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEDATETIME);
+		$message->insert($this->getPDO());
+
+		$media = new Media("817277825208094720", $message->getMessageId(), "image/jpeg", "https://pbs.twimg.com/media/C8cBt21UwAEPdmt.jpg");
+		$media->insert($this->getPDO());
+
+		$awayTeam = Message::emergencyBeamOut($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("message"));
+		$this->assertCount(1, $awayTeam);
+
+		foreach($awayTeam as $pdoMessage) {
+			$this->assertEquals($pdoMessage->getMessageId(), $message->getMessageId());
+			$this->assertEquals($pdoMessage->getMessageProfileId(), $message->getMessageProfileId());
+			$this->assertEquals($pdoMessage->getMessageContent(), $message->getMessageContent());
+			$this->assertEquals($pdoMessage->getMessageDateTime(), $message->getMessageDateTime());
+
+			$this->assertCount(2, $awayTeam[$pdoMessage]);
+			$this->assertInstanceOf("Deepdivedylan\\DylanAndTonyFiesta\\Profile", $awayTeam[$pdoMessage]["profile"]);
+			$this->assertInstanceOf("Deepdivedylan\\DylanAndTonyFiesta\\Media", $awayTeam[$pdoMessage]["media"]);
+		}
 	}
 
 	/**
