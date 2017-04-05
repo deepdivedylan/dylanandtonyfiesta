c2e40c1e8ded8b63850b22cb05d8cffe45ca6e5f
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-04 22:06:18 -0600

    uninstalled Angular :crying_cat_face:

--- src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-	selector: "dylanandtonyfiesta-app",
-	templateUrl: "./templates/dylanandtonyfiesta-app.php"
-})
-
-export class AppComponent {}
