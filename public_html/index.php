5a8b0a4fc8a36d5224b9d0ed8dc3c45129f4d378
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-05 17:12:52 -0600

    loaded instagram feed

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,13 @@ $TITLE = "Dylan and Tony Fiesta - Home";
 <body>
 	<?php require("partials/header.php"); ?>
 	<div class="container">
-		<div id="twitterFeed" class="col-md-6 twitter feed">
+		<div class="col-md-6 twitter feed">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="section-header">Twitter</h2>
+				</div>
+			</div>
+			<div id="twitterFeed"></div>
 		</div>
 		<div class="col-md-6 instagram feed">
 			<?php require("partials/instagram-feed/instagram-feed.php"); ?>
