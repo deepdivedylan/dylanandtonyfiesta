e2dae9deb18e8f0438a970393d7f9be1c5dde1a7
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-05 18:03:28 -0600

    refactored the quote partials - less modular but simpler - meh

--- public_html/partials/quotes/quotes.php
+++ public_html/partials/quotes/quotes.php
@@ -6,17 +6,24 @@
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<?php require("quote-left.php"); ?>
+			<blockquote class="quote">
+				<p>Of all the people I know, Dylan is the most intense and sincere. His emotions are fierce and free, and he cares openly and often. I've never met another person like him and I'm honored to be a part of this milestone in his and Tony's life.</p>
+				<footer><cite>Skyler Rexroad</cite></footer>
+			</blockquote>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<?php require("quote-right.php"); ?>
+			<blockquote class="quote blockquote-reverse">
+				<?php require("quote.php"); ?>
+			</blockquote>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<?php require("quote-left.php"); ?>
+			<blockquote class="quote">
+				<?php require("quote.php"); ?>
+			</blockquote>
 		</div>
 	</div>
 </div>
