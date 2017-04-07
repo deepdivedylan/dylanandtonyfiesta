e0bd5a34df553666f817c9be106ee1bb8a09059b
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-07 07:19:06 -0600

    completed quotes

--- public_html/partials/quotes/quotes.php
+++ public_html/partials/quotes/quotes.php
@@ -15,14 +15,8 @@
 	<div class="row">
 		<div class="col-md-12">
 			<blockquote class="quote blockquote-reverse">
-				<?php require("quote.php"); ?>
-			</blockquote>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<blockquote class="quote">
-				<?php require("quote.php"); ?>
+				<p>I have know you for the past 2-3 years.  I must say that there is never a dull moment.  At most its the shade, or in your tearms read, that makes it wonderful.  Nevertheless there is that part of you that I am able to witness.  The amount of joy when you or me talk about cats.  Your appreciation of Scooby-Doo and how a scooby sandwich is always necessary.  But the one true thing that I witness is, when you mention Dylan or talk about Dylan, I can see a different side of you.  Something that is difficult to explain but is wonderful to witness.</p>
+				<footer><cite>Craig Collins</cite></footer>
 			</blockquote>
 		</div>
 	</div>
