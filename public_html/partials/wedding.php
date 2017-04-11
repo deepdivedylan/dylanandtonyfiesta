f7af932daa33b7d3d9f39a0acaf48d03b3143150
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-11 00:47:42 -0600

    completed wedding page

--- public_html/partials/wedding.php
+++ public_html/partials/wedding.php
@@ -1,9 +1,18 @@
 <div class="container">
 	<div class="row">
+		<h2 class="section-header">Ceremony Video</h2>
+		<div class="embed-responsive embed-responsive-16by9">
+			<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GDwMzSmmK2k" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="row">
 		<h2 class="section-header">Date, Time, &amp; Venue</h2>
+		<h3>Date &amp; Time</h3>
+		<p>2017-04-08 11:00:00 MDT</p>
+		<h3>Venue</h3>
 		<p>
-			First Unitarian Church<br />
-			3701 Carlisle Blvd NE<br />
+			First Unitarian Church<br/>
+			3701 Carlisle Blvd NE<br/>
 			Albuquerque, NM 87102
 		</p>
 	</div>
