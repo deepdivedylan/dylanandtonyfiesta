c9fab44df4f930ced0ca0c4c2299eb130ce70f3e
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-11 00:32:10 -0600

    added wedding page

---
+++ public_html/partials/wedding.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row">
+		<h2 class="section-header">Date, Time, &amp; Venue</h2>
+		<p>
+			First Unitarian Church<br />
+			3701 Carlisle Blvd NE<br />
+			Albuquerque, NM 87102
+		</p>
+	</div>
+</div>
---
+++ public_html/wedding.php
@@ -0,0 +1,12 @@
+<?php
+$TITLE = "Dylan and Tony Fiesta - Wedding";
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<?php require("partials/head.php"); ?>
+	<body>
+		<?php require("partials/header.php"); ?>
+		<?php require("partials/wedding.php"); ?>
+		<?php require("partials/footer.php"); ?>
+	</body>
+</html>
